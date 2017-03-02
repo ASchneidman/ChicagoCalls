f2c53d10a8cfb54f6bc05ca7ff0e41c784881eaf
ASchneidman <alexmarkschneidman@gmail.com>
2017-03-01 19:37:48 -0600

    Implemented a form using POST with Flask to get the user's address

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask('server')
 
 
@@ -6,7 +6,20 @@ app = Flask('server')
 def webprint():
    return render_template('index.html')
 
+@app.route('/',methods = ['POST', 'GET'])
+def alderman():
+    if request.method == 'POST':
+        alder = request.form["address"]
+        return redirect(url_for('success',alderman = alder))
+    else:
+        alder = request.args.get("address")
+        return redirect(url_for('success', alderman = alder))
+
+
+@app.route('/success/<alderman>')
+def success(alderman):
+    return 'Your address is %s' % alderman
+
 if __name__ == '__main__':
    app.debug = True
-   app.run()
    app.run(debug=True)
